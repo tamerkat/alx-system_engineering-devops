34332c6f855f6b591dae0b836668c1baf2ab2ef1
tamerkat <mahad10taha@gmail.com>
2024-05-20 20:26:26 +0300

    api task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,35 +12,24 @@ def shown():
     users = requests.get("http://jsonplaceholder.typicode.com/users")
     for user in users.json():
         if user.get('id') == int(argv[1]):
-            EMPLOYEE_NAME = (user.get('name'))
+            USERNAME = (user.get('username'))
             break
 
-    TOTAL_NUM_OF_TASKS = 0
-    NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
 
     todos = requests.get("http://jsonplaceholder.typicode.com/todos")
     for todo in todos.json():
         if todo.get('userId') == int(argv[1]):
-            TOTAL_NUM_OF_TASKS += 1
             if todo.get('completed') is True:
-                NUMBER_OF_DONE_TASKS += 1
                 TASK_TITLE.append(todo.get('title'))
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUM_OF_TASKS))
-
-    for task in TASK_TITLE:
-        print("\t {}".format(task))
 
     filename = "{}.csv".format(argv[1])
     with open(filename, mode="w") as csvfile:
         for todo in todos:
             complete = todo.get('completed')
             title = todo.get('title')
-            user_name = users.get('username')
             csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, user_name, complete, title))
+                user, USERNAME, complete, title))
 
 
 if __name__ == "__main__":
