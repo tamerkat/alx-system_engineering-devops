2ff6d4c21412dea94ccbe38c5fafedd82be2ae91
tamerkat <mahad10taha@gmail.com>
2024-05-20 21:15:27 +0300

    api task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
-'''
-Write a Python script
-'''
+"""
+Extend script from Task 0 and Export data in CSV format
+"""
 import csv
 import requests
 from sys import argv
 
 
-def shown():
+def to_csv():
     """return API data"""
     users = requests.get("http://jsonplaceholder.typicode.com/users")
     for user in users.json():
         if user.get('id') == int(argv[1]):
             USERNAME = (user.get('username'))
             break
-
-    TASK_TITLE = []
-
+    TASK_STATUS_TITLE = []
     todos = requests.get("http://jsonplaceholder.typicode.com/todos")
     for todo in todos.json():
         if todo.get('userId') == int(argv[1]):
-            TASK_TITLE.append(todo.get('completed'),
-                              todo.get('title'))
+            TASK_STATUS_TITLE.append((todo.get('completed'),
+                                      todo.get('title')))
 
+    """export to csv"""
     filename = "{}.csv".format(argv[1])
-    with open(filename, mode="w") as cf:
-        csvfile = csv.writer(cf)
-        csvfile.writerow(["USER_ID", "USERNAME",
-                          "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in TASK_TITLE:
-            csvfile.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
-                              "TASK_COMPLETED_STATUS": task['completed'],
-                              "TASK_TITLE": task['title']})
+    with open(filename, "w") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+        for task in TASK_STATUS_TITLE:
+            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
+                             "TASK_COMPLETED_STATUS": task[0],
+                             "TASK_TITLE": task[1]})
 
 
 if __name__ == "__main__":
-    shown()
+    to_csv()
