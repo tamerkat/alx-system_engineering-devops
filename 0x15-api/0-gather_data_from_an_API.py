515bff78ad0e52492051758793a689ba53ee27d8
tamerkat <mahad10taha@gmail.com>
2024-05-20 12:17:02 +0300

    Airbnb task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -20,12 +20,12 @@ if __name__ == "__main__":
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
     TASK_TITLE = []
-    
+
     url = 'https://jsonplaceholder.typicode.com/todos/1'
     todo = urllib.request.urlopen(url)
 
     for task in todo:
-        if task['completed'] == True:
+        if task['completed'] == 'True':
             NUMBER_OF_DONE_TASKS += 1
             TOTAL_NUMBER_OF_TASKS += 1
             TASK_TITLE.append(task['title'])
@@ -34,6 +34,6 @@ if __name__ == "__main__":
 
     avrage = NUMBER_OF_DONE_TASKS / TOTAL_NUMBER_OF_TASKS
     print("Employee {} is done with tasks({}):".format(EMPLOYEE_NAME, avrage))
-    
+
     for task in TASK_TITLE:
         print("\t {}".format(task))
