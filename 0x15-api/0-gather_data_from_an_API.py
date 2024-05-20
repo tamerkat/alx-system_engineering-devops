96dcc0a5d50d95ee20e81a57dae5fb7ac3a281aa
tamerkat <mahad10taha@gmail.com>
2024-05-20 13:55:38 +0300

    Airbnb task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,36 +4,36 @@ display
 '''
 
 import sys
-import urllib
+import requests
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/users'
-    users = urllib.request.urlopen(url)
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
 
     usr_id = sys.argv[1]
 
     for user in users.json():
-        if user['id'] == usr_id:
+        if user.get('id') == usr_id:
             EMPLOYEE_NAME = users['name']
             break
 
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
     TASK_TITLE = []
-
-    url = 'https://jsonplaceholder.typicode.com/todos/1'
-    todo = urllib.request.urlopen(url)
+    
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    todo = requests.get(url)
 
     for task in todo:
-        if task['completed'] == 'True':
+        if task['completed'] == "True":
             NUMBER_OF_DONE_TASKS += 1
             TOTAL_NUMBER_OF_TASKS += 1
-            TASK_TITLE.append(task['title'])
+            TASK_TITLE.append(task.get('title'))
         else:
             TOTAL_NUMBER_OF_TASKS += 1
+            TASK_TITLE.append(task.get('title'))
 
     avrage = NUMBER_OF_DONE_TASKS / TOTAL_NUMBER_OF_TASKS
     print("Employee {} is done with tasks({}):".format(EMPLOYEE_NAME, avrage))
-
+    
     for task in TASK_TITLE:
         print("\t {}".format(task))
